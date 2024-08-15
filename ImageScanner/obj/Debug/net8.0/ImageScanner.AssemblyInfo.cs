05c568b2af6bad163fd1995f171f492d293ca131
Naila BOUADIL <nbouadil@gmail.com>
2024-08-15 02:05:57 +0000

    code with error from test branch

--- ImageScanner/obj/Debug/net8.0/ImageScanner.AssemblyInfo.cs
+++ ImageScanner/obj/Debug/net8.0/ImageScanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImageScanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e25e7aa509a6caa822c7023840e5e38d04c62c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945685e3189d818d4b8c7101a7535768edee298c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImageScanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImageScanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
